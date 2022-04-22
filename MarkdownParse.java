512f8ca09b3010b39fd79703f81d505103c68f99
333GUSSS <gbaez@ucsd.edu>
2022-04-22 10:50:28 -0700

    Added Test File and ran these two commands. Keep in mind that the lib directory is not getting pushed.
    javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
    java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,43 +9,53 @@ public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-
-        ArrayList<String> empty_link = new ArrayList<>();
-
-        //find the next [, then find the ], then find the (, then read link upto next )
+        // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        
-        if (markdown.length() == 0) {
-            System.out.print("There are no links in the file.Thus, you will recieve an empty pair of brackets");
-        }
-
-        System.out.println("file total length = " + markdown.length());
-        
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
-            System.out.println("26: currentIndex = " + currentIndex + "\n" + "26: openBracket = " + openBracket);
-
-            int closeBracket = markdown.indexOf("]", openBracket);
-            System.out.println("29: closeBracket = " + closeBracket);
-
+	    if (openBracket == -1) {
+		    break;
+	    }
+            int closeBracket = findBraces(markdown, openBracket, '[', ']'); 
             int openParen = markdown.indexOf("(", closeBracket);
-            System.out.println("32: openParen = " + openParen);
-
-            int closeParen = markdown.indexOf(")", openParen);
-            System.out.println("35: closeParen = " + closeParen);
-
+            int closeParen = findBraces(markdown, openParen, '(', ')');
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            currentIndex = closeParen + 2;
         }
-
         return toReturn;
     }
 
+    public static int findBraces(String markdown, int aStart, char aChar, char aCloseChar) {
+	int openBrace = markdown.indexOf(aChar, aStart);
+	int openCount = 0;
+	int i = openBrace + 1;
+	while (true) {
+		if (i >= markdown.length()) {
+			return -1;
+		}
+		char theCurr = markdown.charAt(i);
+		if (theCurr == aCloseChar) {
+			if (0 == openCount) {
+				return i;
+			} else {
+				--openCount;
+			}
+		} else if (theCurr == aChar) {
+			++openCount;
+		}
+		++i;
+	}
+    }
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+	if (0 == links.size()) {
+		System.out.println("No links found");
+	} else {
+		System.out.println(links);
+	}
+
     }
 }
