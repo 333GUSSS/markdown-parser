b8b225d8e9b478c3599bf0c1825a79ca6c9f3a4e
333GUSSS <gbaez@ucsd.edu>
2022-05-24 11:30:53 -0700

    Lab Report 4 Test Snippets

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,6 +7,27 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class MarkdownParseTest {
+
+    @Test
+    public void testSnippet1() throws IOException {
+        assertEquals(List.of("url.com", "`google.com", "ucsd.edu"), MarkdownParse.getLinks(Files.readString(Path.of("test-snippet-1.md"))));
+    }
+    
+    @Test
+    public void testSnippet2() throws IOException {
+        assertEquals(List.of("b.com", "a.com(())", "example.com"), MarkdownParse.getLinks(Files.readString(Path.of("test-snippet-2.md"))));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        assertEquals(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(Files.readString(Path.of("test-snippet-3.md"))));
+    }
+    
+    
+}
+
+
+/*
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
@@ -16,16 +37,10 @@ public class MarkdownParseTest {
     @Test
     public void testFile() throws IOException {
         assertEquals(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
-        //assertEquals(Arraylist, MarkdownParse.getLinks(Files.readString(Path.of("test-file-empty.md"))));
-        //assertEquals(List.of("(https://studentaid.gov/h/apply-for-aid/fafsa)"), MarkdownParse.getLinks(Files.readString(Path.of("test-file-nestedParanthesis.md"))));
-        //assertEquals(List.of("https://www.youtube.com/, https://code.visualstudio.com/"), MarkdownParse.getLinks(Files.readString(Path.of("test-file-normalLinks.md"))));
     }
 
     @Test
     public void testFile2() throws IOException {
-        //assertEquals(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
-        //assertEquals(Arraylist, MarkdownParse.getLinks(Files.readString(Path.of("test-file-empty.md"))));
-        //assertEquals(List.of("(https://studentaid.gov/h/apply-for-aid/fafsa)"), MarkdownParse.getLinks(Files.readString(Path.of("test-file-nestedParanthesis.md"))));
-	assertEquals(List.of("https://www.youtube.com/", "https://code.visualstudio.com/"), MarkdownParse.getLinks(Files.readString(Path.of("test-file-normalLinks.md"))));
+	    assertEquals(List.of("https://www.youtube.com/", "https://code.visualstudio.com/"), MarkdownParse.getLinks(Files.readString(Path.of("test-file-normalLinks.md"))));
     }
-}
+    */
